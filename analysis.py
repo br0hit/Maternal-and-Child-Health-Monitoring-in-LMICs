cf439a473e04fdcae733ac69dfce833b7be7f147
br0hit <rohit.bannuru@gmail.com>
2023-06-14 14:38:30 +0530

    created file for basic model

--- analysis.py
+++ analysis.py
@@ -1,12 +1,21 @@
 import pandas as pd
 
-# Load the gee_features dataset
-gee_features = pd.read_csv('../data/gee_features.csv', nrows=10000)
-
-# Load the sample submission file
-sample_submission = pd.read_csv('../data/sample_submission.csv')
-
-# Load the training labels
+# Loading datasets 
+gee_features = pd.read_csv('../data/gee_features.csv', nrows=100)
 training_labels = pd.read_csv('../data/training_label.csv')
+# sample_submission = pd.read_csv('../data/sample_submission.csv')
 
-print(gee_features['DHSID'])
+# Check missing values in training labels dataset
+missing_values = training_labels.isnull().sum()
+print(missing_values)
+print("\n\n\n")
+
+# Check missing values in gee features dataset
+missing_values = gee_features.isnull().sum()
+print(missing_values)
+
+
+
+
+
+
